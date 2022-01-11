6a0ed88136418c266be6444c0dc1a30b807ebe19
Rhamanda Lima <cathyannarhamanda@gmail.com>
2022-01-11 17:30:39 -0300

    Break e debbuger

--- projeto-atual/8-loops.js
+++ projeto-atual/8-loops.js
@@ -13,15 +13,15 @@ const destino = "Curitiba";
 console.log("\n Destinos possíveis:");
 console.log(listaDeDestinos);
 
-const podeComprar  = idadeComprador >= 18 || estaAcompanhada == true;
+const podeComprar = idadeComprador >= 18 || estaAcompanhada == true;
 
 let contador = 0;
+let destinoExiste = false;
 
-while(contador<3){
-    if(listaDeDestinos[contador] == destino){
-        console.log("Destino existe") 
-    }else{ 
-        console.log("Destino não existe");
+while (contador < 3) {
+    if (listaDeDestinos[contador] == destino) {
+        destinoExiste = true;
+        break;
     }
     contador += 1;
-} 
+}
